fa22876fc6a8aadea8992a7ab461c4fb335d786d
grzeski <grzegorz.rzeski@autonomik.pl>
2020-11-03 18:08:49 +0100

    unskipped test 18

--- src/zad2/tests.py
+++ src/zad2/tests.py
@@ -53,7 +53,7 @@ class RomanNumeralsTest(unittest.TestCase):
 
     def test_900_being_1000_100_is_cm(self):
         self.assertEqual(Roman.roman(911), "CMXI")
-    @unittest.skip("skipping")
+
     def test_1000_is_a_single_m(self):
         self.assertEqual(Roman.roman(1024), "MXXIV")
     @unittest.skip("skipping")
