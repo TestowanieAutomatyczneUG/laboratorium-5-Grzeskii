eddf5e786a76c9172f1b57b839ecfbbac6a24d06
grzeski <grzegorz.rzeski@autonomik.pl>
2020-11-03 13:01:40 +0100

    unskipped all other tests, all passing

--- src/zad1/tests.py
+++ src/zad1/tests.py
@@ -21,15 +21,15 @@ class HammingTest(unittest.TestCase):
     def test_disallow_first_strand_longer(self):
         with self.assertRaisesWithMessage(ValueError):
             hamming.distance("AATG", "AAA")
-    @unittest.skip("skipping")
+    
     def test_disallow_second_strand_longer(self):
         with self.assertRaisesWithMessage(ValueError):
             hamming.distance("ATA", "AGTG")
-    @unittest.skip("skipping")
+    
     def test_disallow_left_empty_strand(self):
         with self.assertRaisesWithMessage(ValueError):
             hamming.distance("", "G")
-    @unittest.skip("skipping")
+    
     def test_disallow_right_empty_strand(self):
         with self.assertRaisesWithMessage(ValueError):
             hamming.distance("G", "")
