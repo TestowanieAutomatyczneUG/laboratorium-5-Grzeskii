e325f46a0d5ca600dd103c935a5b57acb997caf8
grzeski <grzegorz.rzeski@autonomik.pl>
2020-11-03 19:28:35 +0100

    code refactor

--- src/zad3/song.py
+++ src/zad3/song.py
@@ -5,20 +5,15 @@ class Song:
         "On the eleventh day of Christmas my true love gave to me: eleven Pipers Piping, ten Lords-a-Leaping, nine Ladies Dancing, eight Maids-a-Milking, seven Swans-a-Swimming, six Geese-a-Laying, five Gold Rings, four Calling Birds, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree.", "On the twelfth day of Christmas my true love gave to me: twelve Drummers Drumming, eleven Pipers Piping, ten Lords-a-Leaping, nine Ladies Dancing, eight Maids-a-Milking, seven Swans-a-Swimming, six Geese-a-Laying, five Gold Rings, four Calling Birds, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree."]
 
     def line(self, line):
-        if line == 1:
-            return self.song[line - 1]
-        if line == 3:
-            return self.song[line - 1]
         if line > 12:
             raise ValueError("Numbers must be between 1 and 12")
         if line <= 0:
             raise ValueError("Numbers must be between 1 and 12")
         if type(line) is not int:
             raise TypeError("Argument must be an int")
+        return self.song[line - 1]
     
     def fewLines(self, line1, line2):
-        if line1 == 2 and line2 == 5:
-            return self.song[line1:line2-1]
         if line1 == line2:
             return []
         if type(line1) is not int or type(line2) is not int:
@@ -27,6 +22,7 @@ class Song:
             raise ValueError("First argument must be smallar than the second argument")
         if line1 <= 0 or line2 <= 0:
             raise ValueError("Arguments must not be negative")
+        return self.song[line1:line2 - 1]
         
     
     def wholeSong(self):
