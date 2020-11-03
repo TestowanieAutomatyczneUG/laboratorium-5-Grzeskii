1e4009544d3eb28fd195c04b84fa29e91e32a3d4
grzeski <grzegorz.rzeski@autonomik.pl>
2020-11-03 17:13:17 +0100

    unskipped test 1

--- src/zad2/tests.py
+++ src/zad2/tests.py
@@ -6,7 +6,7 @@ class RomanNumeralsTest(unittest.TestCase):
     def setUp(self):
         self.temp = roman()
     
-    @unittest.skip("skipping")
+
     def test_1_is_a_single_i(self):
         self.assertEqual(roman(1), "I")
     @unittest.skip("skipping")
