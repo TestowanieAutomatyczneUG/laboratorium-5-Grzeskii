18ec6eb66d617b81ed76cfc227d801f161923beb
grzeski <grzegorz.rzeski@autonomik.pl>
2020-11-03 13:00:39 +0100

    implementation for test 7

--- src/zad1/hamming.py
+++ src/zad1/hamming.py
@@ -1,6 +1,8 @@
 class Hamming:
     def distance(first, second):
         numberOfErrors = 0
+        if len(first) != len(second):
+            raise ValueError("Strands should be the same length")
         if first == "" and second == "":
             return 0
         if first == second:
