a95ed51a6c9201a4031f147e0101f494c8f09251
grzeski <grzegorz.rzeski@autonomik.pl>
2020-11-03 18:06:06 +0100

    unskipped test 14

--- src/zad2/tests.py
+++ src/zad2/tests.py
@@ -41,7 +41,7 @@ class RomanNumeralsTest(unittest.TestCase):
 
     def test_100_is_a_single_c(self):
         self.assertEqual(Roman.roman(141), "CXLI")
-    @unittest.skip("skipping")
+
     def test_60_being_50_10_is_lx(self):
         self.assertEqual(Roman.roman(163), "CLXIII")
     @unittest.skip("skipping")
