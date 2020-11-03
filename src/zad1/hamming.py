5b5c0a9b6e6259c405c6b242bbce8a395c5e10c3
grzeski <grzegorz.rzeski@autonomik.pl>
2020-11-03 12:39:40 +0100

    implementation for test 6

--- src/zad1/hamming.py
+++ src/zad1/hamming.py
@@ -1,9 +1,11 @@
 class Hamming:
-    
     def distance(first, second):
+        numberOfErrors = 0
         if first == "" and second == "":
             return 0
         if first == second:
             return 0
-        if first != second:
-            return 1
+        for i in range(len(first)): 
+            if first[i] != second[i]:
+                numberOfErrors += 1
+        return numberOfErrors
