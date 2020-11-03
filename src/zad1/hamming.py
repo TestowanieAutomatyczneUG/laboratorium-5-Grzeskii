fbcbc3886a8e37bb31d0fabe86f6653e63c917fb
grzeski <grzegorz.rzeski@autonomik.pl>
2020-11-03 17:03:34 +0100

    finish zad 1

--- src/zad1/hamming.py
+++ src/zad1/hamming.py
@@ -3,10 +3,6 @@ class Hamming:
         numberOfErrors = 0
         if len(first) != len(second):
             raise ValueError("Strands should be the same length")
-        # if first == "" and second == "":
-        #     return 0
-        # if first == second:
-        #     return 0
         for i in range(len(first)): 
             if first[i] != second[i]:
                 numberOfErrors += 1
